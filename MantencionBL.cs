f1e10903b3dcf1d90c8877d8715fdc33762db26c
FelipeEngineerTI <67295920+FelipeEngineerTI@users.noreply.github.com>
2020-06-22 19:31:58 -0400

    BL
    Código de la lógica de negocios del proyecto.

---
+++ MantencionBL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTPS.BL
+{
+    [DataObject]
+    public class MantencionBL
+    {
+        Database1Entities1 db = new Database1Entities1();
+        [DataObjectMethod(DataObjectMethodType.Insert)]
+        public void AgregarMantencion(string tituloMantencion, DateTime fechaEmision, DateTime fechaRealizacion, TimeSpan hora, string responsableMantencion, string tipoMantencion, string obsMantencion, string descMantencion)
+        {
+            db.Mantencion.Add(new Mantencion() { TituloMantencion = tituloMantencion, FechaEmisionMantencion = fechaEmision, FechaRealizacionMantencion = fechaRealizacion, HoraMantencion = hora, Responsable = responsableMantencion, TipoMantencion = tituloMantencion, ObservacionMantencion = obsMantencion, DescripcionMantencion = descMantencion });
+            db.SaveChanges();
+        }
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Mantencion> GetMantenciones()
+        {
+            return (from m in db.Mantencion select m).ToList();
+
+        }
+        [DataObjectMethod(DataObjectMethodType.Update)]
+        public Mantencion EditarMantencion( int idMantencion)
+        {
+            return (from m in db.Mantencion where m.IdMantencion == idMantencion select m).FirstOrDefault();
+        }
+        
+        
+    }
+}
---
+++ ResponsableBL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+namespace ProyectoTPS.BL
+{
+    [DataObject]
+    public class ResponsableBL
+    {
+        Database1Entities1 db = new Database1Entities1();
+        [DataObjectMethod(DataObjectMethodType.Insert)]
+        public void AgregarResponsable(string nombreResponsable, string apellidoResponsable, string rutResponsable,string rolResponsable, int celularResponsable, string correoResponsable)
+        {
+            db.Responsable.Add(new Responsable() { NombreResponsable = nombreResponsable, ApellidoResponsable = apellidoResponsable, RutResponsable = rutResponsable, RolResponsable = rolResponsable, CelularResponsable = celularResponsable, CorreoResponsable = correoResponsable });
+            db.SaveChanges();
+        }
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Responsable> GetResponsablesPorMantencion(int idMantencion)
+        {
+            return (from r in db.Responsable where r.Mantencion.IdMantencion == idMantencion select r).ToList();
+
+        }
+        [DataObjectMethod(DataObjectMethodType.Select)]
+        public List<Responsable> GetResponsables()
+        {
+            return (from rp in db.Responsable select rp).ToList();
+        }
+
+    }
+}
